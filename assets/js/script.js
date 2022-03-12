512f3d6b0b5c26c4fc61a2c9be6ce4d75636abf5
Michael Kendrick <michael@locumsmart.net>
2022-03-12 12:17:13 -0600

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,11 @@ var fallLimit = document.querySelector(".fallLimit");
 var price = document.querySelector(".price");
 var images = document.querySelector(".images");
 var lightbox = document.querySelector(".lightbox");
+var lightboxModal = document.querySelector("#lightboxModal");
+var mainImgEl = document.createElement("img");
+var mainImg = img1.dataset.image;
+var mainImgUrl = img1.src;
+console.log(mainImg);
 
 plusBtn.addEventListener("click", function () {
   count.value++;
@@ -26,26 +31,43 @@ minusBtn.addEventListener("click", function () {
   count.value--;
 });
 
-thumb1.addEventListener("click", function () {
-  thumb1.classList.add("inFocus");
-  thumb2.classList.remove("inFocus");
-  thumb3.classList.remove("inFocus");
-  thumb4.classList.remove("inFocus");
-  img1.classList.remove("hidden");
-  img2.classList.add("hidden");
-  img3.classList.add("hidden");
-  img4.classList.add("hidden");
+thumb1.addEventListener("click", function (event) {
+  console.log(event.target.dataset.image);
+  if (event.target.dataset.image !== mainImg) {
+    mainImg = img1.dataset.image;
+    images.removeChild();
+    mainImgEl.src = img1.src;
+    images.append(mainImgEl);
+    console.log(true);
+  }
+  // thumb1.classList.add("inFocus");
+  // thumb2.classList.remove("inFocus");
+  // thumb3.classList.remove("inFocus");
+  // thumb4.classList.remove("inFocus");
+  // img1.classList.remove("hidden");
+  // img2.classList.add("hidden");
+  // img3.classList.add("hidden");
+  // img4.classList.add("hidden");
 });
 
-thumb2.addEventListener("click", function () {
-  thumb2.classList.add("inFocus");
-  thumb1.classList.remove("inFocus");
-  thumb3.classList.remove("inFocus");
-  thumb4.classList.remove("inFocus");
-  img1.classList.add("hidden");
-  img2.classList.remove("hidden");
-  img3.classList.add("hidden");
-  img4.classList.add("hidden");
+thumb2.addEventListener("click", function (event) {
+  if (event.target.dataset.image !== mainImg) {
+    mainImg = img2.dataset.image;
+    images.removeChild(img1);
+    mainImgEl.src = img2.src;
+    images.append(mainImgEl);
+    mainImgEl.classList.add("productImg2");
+    thumb2.classList.add("inFocus");
+    console.log(true);
+  }
+  //   thumb2.classList.add("inFocus");
+  //   thumb1.classList.remove("inFocus");
+  //   thumb3.classList.remove("inFocus");
+  //   thumb4.classList.remove("inFocus");
+  //   img1.classList.add("hidden");
+  //   img2.classList.remove("hidden");
+  //   img3.classList.add("hidden");
+  //   img4.classList.add("hidden");
 });
 
 thumb3.addEventListener("click", function () {
@@ -92,7 +114,11 @@ cartBtn.addEventListener("click", function () {
     parseInt(price.innerText.split("$")[1]) * count.value;
 });
 
-// images.addEventListener("click", function () {
-//   lightbox.classList.add("lightboxModal");
-//   console.log("pic");
-// });
+images.addEventListener("click", function (event) {
+  lightboxModal.classList.remove("hidden");
+  var lightboxImg = document.createElement("img");
+  lightboxImg.src = event.target.src;
+  lightboxModal.appendChild(lightboxImg);
+  // lightbox.classList.add("lightboxModal");
+  console.log(event.target.src);
+});
